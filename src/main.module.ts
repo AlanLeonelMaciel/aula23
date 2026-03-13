ffd1f2821bdb7a5025a4cec325236278e005e8a5
alan <alanleonelmaciel@gmail.com>
2026-03-13 00:35:10 -0300

    lala

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
+
 })
-export class AppModule {}
+export class AppModule { }
--- src/presentation/controllers/health/healthcheck.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class HealthCheckService {
-  checkHealth(): Promise<string> {
-    return Promise.resolve('healthy');
-  }
-}
